fb0a17992cca9037dfb43611e968fcc510c1229a
Vitor Hugo Marcal <vitorhugomarcal@gmail.com>
2024-02-07 21:53:01 -0300

    initial commit

---
+++ src/app/cart.tsx
@@ -0,0 +1,116 @@
+import { Alert, Linking, ScrollView, Text, View } from "react-native";
+
+import { ProductCartProps, useCartStore } from "@/stores/cart-store";
+
+import { Header } from "@/components/header";
+import { Product } from "@/components/product";
+import { formatCurrency } from "@/utils/functions/format-currency";
+import { Input } from "@/components/input";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+import { LinkButton } from "@/components/link-button";
+import { useState } from "react";
+import { add } from "@/stores/helpers/cart-in-memory";
+import { useNavigation } from "expo-router";
+
+const PHONE_NUMBER = "5511946202703"
+
+export default function Cart() {
+  const { goBack } = useNavigation()
+  const [address, setAddress] = useState('')
+  const cartStore = useCartStore()
+
+  const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.price * product.quantity, 0))
+
+  function handleProductRemove(product: ProductCartProps) {
+    Alert.alert('Remover', `Deseja remover ${product.title} do carrinho?`,[
+      {
+        text: "Cancelar"
+      },
+      {
+        text: "Remover",
+        onPress: () => cartStore.remove(product.id)
+      }
+    ])
+  }
+
+  function handleOrder() {
+    if (address.trim().length === 0) {
+      return Alert.alert('Pedido', 'Informe os dados da entrega.')
+    }
+
+    const products = cartStore.products
+      .map((product) => `\n ${product.quantity} ${product.title}`)
+      .join('')
+
+      const message = `
+      🍔 NOVO PEDIDO
+      \n Entregar em ${address}
+
+      ${products}
+
+      \n Valor total: ${total}
+      `
+
+      Linking.openURL(`http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`)
+
+      cartStore.clear()
+      goBack()
+      
+  }
+
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Seu carrinho"/>
+      <KeyboardAwareScrollView>
+        <ScrollView>
+          <View className="p-5 flex-1">
+        {
+          cartStore.products.length > 0 ?
+          <View className="border-b border-slate-700">
+            {
+              cartStore.products.map((product) => (
+                <Product key={product.id} data={product} onPress={() => handleProductRemove(product)}/>
+                ))
+            }
+          </View>
+        :
+          <Text className="font-body text-slate-400 text-center my-8">
+            Seu carrinho está vazio.
+          </Text>
+        }
+
+
+        <View className="flex-row gap-2 items-center mt-5 mb-4">
+          <Text className="text-white text-xl font-subtitle">Total:</Text>
+          <Text className="text-lime-400 text-2xl font-heading">
+            {total}
+          </Text>
+        </View>
+
+        <Input 
+          placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..."
+          onChangeText={setAddress}
+          onSubmitEditing={handleOrder}
+          blurOnSubmit
+          returnKeyType="send"
+        />
+        </View>
+        </ScrollView>
+      </KeyboardAwareScrollView>
+
+      <View className="p-5 gap-5">
+        <Button onPress={handleOrder}>
+          <Button.Text>Enviar pedido</Button.Text>
+          <Button.Icon>
+            <Feather name="arrow-right-circle" size={20}/>
+          </Button.Icon>
+        </Button>
+
+        <LinkButton title="Voltar ao cardápio" href="/"/>
+      </View>
+      
+    </View>
+  )
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,69 @@
+import { useState, useRef } from 'react'
+import { View, FlatList, SectionList, Text } from 'react-native'
+
+import { Header } from '@/components/header'
+import { Product } from '@/components/product'
+import { CategoryButton } from '@/components/category-button'
+
+import { CATEGORIES, MENU, ProductProps } from '@/utils/data/products'
+import { Link } from 'expo-router'
+import { useCartStore } from '@/stores/cart-store'
+
+export default function Home() {
+  const cartStore = useCartStore()
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  const sectionListRef = useRef<SectionList<ProductProps>>(null)
+
+  const cartQuantituItems = cartStore.products.reduce((total, product) => total + product.quantity, 0)
+  
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory)
+
+    const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0,
+      })
+    }
+  }
+
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantityItems={cartQuantituItems} />
+
+      <FlatList 
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({item}) => (
+          <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)}/>
+        )}
+        horizontal
+        className='max-h-10 mt-5'
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20}}
+        showsHorizontalScrollIndicator={ false }
+      />
+
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title }}) => 
+          <Text className='text-xl text-white font-heading mt-8 mb-3'>{title}</Text>
+        }
+        className='flex-1 p-5'
+        showsVerticalScrollIndicator={ false }
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />
+    </View>
+  )
+}
